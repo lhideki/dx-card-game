f75775d6d521074c6c7af3af6dd10e28f09bc2b1
621 <hideki@inoue-kobo.com>
2025-05-28 16:24:35 +0900

    Fix leftover merge conflict markers

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-=======
-  experimental: {
-    appDir: true,
-  },
->>>>>>> origin/main
+  
 };
 
 export default nextConfig;
